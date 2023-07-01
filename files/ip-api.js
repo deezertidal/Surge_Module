1861aed45db6cce8bb5ae684c33822fd1956a1ad
deezertidal <jrull5400@gmail.com>
2023-07-01 17:15:06 +0800

    Create ip-api.js

---
+++ files/ip-api.js
@@ -0,0 +1,32 @@
+let url = "http://ip-api.com/json/?fields=8450015&lang=zh-CN"
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+	let query =jsonData.query 
+	let isp =jsonData.isp
+	let as =jsonData.as
+	let country =jsonData.country
+	let city =jsonData.city
+	let timezone =jsonData.timezone
+	let lon =jsonData.lon
+	let lat =jsonData.lat
+	let currency =jsonData.currency
+    let emoji = getFlagEmoji(jsonData.countryCode)
+
+  body = {
+    title: "节点信息",
+    content: `🗺️IP：${query}\n🖥️ISP：${isp}\n#️⃣ASN：${as}\n🌍国家/地区：${emoji}${country}\n🏙城市：${city}\n🕗时区：${timezone}\n📍经纬度：${lon},${lat}\n🪙货币：${currency}`,
+    icon: "mappin.and.ellipse"
+  }
+  $done(body);
+});
+
+function getFlagEmoji(countryCode) {
+      if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+}
