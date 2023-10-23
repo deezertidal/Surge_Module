4d016e379aaf66f6176b096f1c2db78f58ec4e22
yfamily <jrull5400@gmail.com>
2023-10-23 18:39:13 +0800

    Update histoday.js

--- files/histoday.js
+++ files/histoday.js
@@ -1,42 +1,45 @@
 const params = getParams($argument);
 
-const url = "https://www.ipip5.com/today/api.php?type=json";
+const url = "https://lishishangdejintian.bmcx.com/";
 
 $httpClient.get(url, (error, response, data) => {
   if (error) {
     console.log(error);
     $done();
   } else {
-    const result = JSON.parse(data);
-    const today = result.today;
-    let events = result.result;
-
-    events = events.filter((event) => event.year !== "2023");
-
-    events.sort((a, b) => parseInt(b.year) - parseInt(a.year));
-
-
-const count = parseInt(params.count) || 5; 
-events = events.slice(0, count);
-
-    if (events.length > 0) {
-      const eventList = events.map((event) => `📓${event.year}年：${event.title}`).join("\n");
-      const notification =`${eventList}`;
-
-      const body = {
-        title: "历史上的"+today,
-        content: notification,
-        icon: params.icon,
-        "icon-color": params.color,
-        count:count
-      };
-
-      $done(body);
-    } else {
-      $done({});
-    }
+    const sanitizedData = data.replace(/&nbsp;/g, ' ');
+    handleResponse(sanitizedData);
   }
 });
+
+function handleResponse(data) {
+  const regex = /(\d{4}年)(\d{1,2}月\d{1,2}日) <a href='\/\d+__lishishangdejintianchaxun\/' target='_blank'>(.*?)<\/a>/g;
+  const matches = [...data.matchAll(regex)];
+
+  if (matches.length > 0) {
+    const today = new Date().getFullYear();
+    const events = [];
+
+    for (const match of matches) {
+      events.push(`${match[1]} ${match[3]}`);
+    }
+
+    const count = parseInt(params.count) || 6;
+    const notification = events.slice(0, count).join("\n");
+
+    const body = {
+      title: "历史上的今天" ,
+      content: notification,
+      icon: params.icon,
+      "icon-color": params.color,
+      count: count
+    };
+
+    $done(body);
+  } else {
+    $done({});
+  }
+}
 
 function getParams(param) {
   return Object.fromEntries(
